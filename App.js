51817de7f795182170ee61017ddf432957418f7c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-09-13 17:38:35 +0900

    1.2 AppLoading part Two

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import * as Font from "expo-font";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./my-face.jpeg"));
+    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+  };
   if (!ready) {
     return (
       <AppLoading
