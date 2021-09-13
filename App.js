69dd4de2b9944f4d08dd44ba3bd2620c9ff6379d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-09-13 18:05:09 +0900

    1.3 AppLoading part Three

--- App.js
+++ App.js
@@ -3,15 +3,31 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [assets] = useAssets([require("./my-face.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./my-face.jpeg"));
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my-face.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
