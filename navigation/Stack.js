24753df03b65b67f966f192652a889adf75ed55e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-09-15 21:35:17 +0900

    1.11 Configuring Stack Navigation

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View, TouchableOpacity } from "react-native";
+import { YELLOW_COLOR } from "../colors";
 
 const ScreenOne = ({ navigation: { navigate } }) => (
   <TouchableOpacity onPress={() => navigate("Two")}>
@@ -21,7 +22,14 @@ const ScreenThree = ({ navigation: { setOptions } }) => (
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => (
-  <NativeStack.Navigator>
+  <NativeStack.Navigator
+    screenOptions={{
+      presentation: "modal",
+      animation: "fade",
+      headerTintColor: YELLOW_COLOR,
+      headerBackTitleVisible: false,
+    }}
+  >
     <NativeStack.Screen name="One" component={ScreenOne} />
     <NativeStack.Screen name="Two" component={ScreenTwo} />
     <NativeStack.Screen name="Three" component={ScreenThree} />
