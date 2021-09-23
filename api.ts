ade5797132424f54a9ec4865d192d0dd63cd7cad
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-09-23 18:30:23 +0900

    2.24 Detail Screen part Four

--- api.ts
+++ api.ts
@@ -74,6 +74,12 @@ export const moviesApi: Fetchers<MovieResponse> = {
       `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
     ).then((res) => res.json());
   },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+    ).then((res) => res.json());
+  },
 };
 
 export const tvApi: Fetchers<TVResponse> = {
@@ -93,6 +99,12 @@ export const tvApi: Fetchers<TVResponse> = {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
     ).then((res) => res.json());
   },
 };
--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -3,10 +3,11 @@ import React, { useEffect } from "react";
 import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Movie, TV } from "../api";
+import { Movie, moviesApi, TV, tvApi } from "../api";
 import Poster from "../components/Poster";
 import { makeImgPath } from "../utils";
 import { BLACK_COLOR } from "../colors";
+import { useQuery } from "react-query";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -49,6 +50,22 @@ const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
+  const { isLoading: moviesLoading, data: moviesData } = useQuery(
+    ["movies", params.id],
+    moviesApi.detail,
+    {
+      enabled: "original_title" in params,
+    }
+  );
+  const { isLoading: tvLoading, data: tvData } = useQuery(
+    ["tv", params.id],
+    tvApi.detail,
+    {
+      enabled: "original_name" in params,
+    }
+  );
+  console.log("movies", moviesData);
+  console.log("tv", tvData);
   useEffect(() => {
     setOptions({
       title: "original_title" in params ? "Movie" : "TV Show",
